df520679133286d50deae1b6bca9fb6e2c74798c
Your Name <nadinekeyrouz0011@gmail.com>
2025-02-27 12:10:46 +0200

    trt

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,26 +2,20 @@
 
 import Head from "../components/header";
 import ButText from "../components/txt";
-import { useState } from "react";
 
 export default function HomePage() {
-  const [aboutContent, setAboutContent] = useState({
+  const aboutContent = {
     title: "Find Your Dream Job Effortlessly",
     description: "Connecting job seekers with top employers worldwide. Explore thousands of opportunities, apply with ease, and take the next step in your career with our advanced job-matching platform.",
-  });
-  
+  };
+
   return (
-    <div className=" ">
-      
-       <Head activeSection="home" />
-       
-       <ButText
-           
-            title={aboutContent.title}
-            description={aboutContent.description}
-         
-          />
-    
+    <div className="">
+      <Head activeSection="home" />
+      <ButText
+        title={aboutContent.title}
+        description={aboutContent.description}
+      />
     </div>
   );
 }
